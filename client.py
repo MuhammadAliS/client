65cb7349b4e3f603ff965596170e9208b5e780c4
Muhammad Ali_S <66350346+MuhammadAliS@users.noreply.github.com>
2020-10-05 10:44:16 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,8 +1,8 @@
 import socket
 c=socket.socket(socket.AF_INET6,socket.SOCK_STREAM)
 ipv6=input("Enter IPV6:")
-port=input('Enter Port Number:')
-c.connect((IPV6,port))
+port=int(input('Enter Port Number:'))
+c.connect((ipv6,port))
 print("Connected to the network")
 print(c.recv(1024).decode())
 username=input("Enter a username:")
